7447dd3c5a93eee371b0dbe643122d25e3ff964a
Marten de Bruijn <martendebruijn@outlook.com>
2025-01-04 05:44:01 +0100

    update: add githubUrl to SectionItem

--- src/components/SectionItem.tsx
+++ src/components/SectionItem.tsx
@@ -1,8 +1,11 @@
+import { ExternalLinkIcon } from "lucide-react";
+
 interface Props {
   title: string;
   description: string;
   image: string;
   imageAlt?: string;
+  githubUrl?: string;
 }
 
 export function SectionItem({
@@ -10,6 +13,7 @@ export function SectionItem({
   description,
   image,
   imageAlt = "",
+  githubUrl,
 }: Props) {
   return (
     <article>
@@ -22,6 +26,17 @@ export function SectionItem({
       </div>
       <h3 className="font-semibold text-lg">{title}</h3>
       <p>{description}</p>
+      {githubUrl && (
+        <a
+          href={githubUrl}
+          className="underline text-blue-500 inline-flex items-center mt-2 gap-2"
+        >
+          <span>GitHub</span>
+          <span>
+            <ExternalLinkIcon />
+          </span>
+        </a>
+      )}
     </article>
   );
 }
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -13,18 +13,21 @@ export function Home() {
           description="Personal common used types, type guards and other to quickly jumpstart a new project or experiment"
           image="https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?q=80&w=3615&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           imageAlt="macbook pro on black wooden table"
+          githubUrl="https://github.com/martendebruijn/types"
         />
         <SectionItem
-          title="TypeScript"
-          description="TypeScript is a typed superset of JavaScript that compiles to plain JavaScript."
+          title="Dotfiles"
+          description="Personal dotfiles for macOS"
           image="https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?q=80&w=3615&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           imageAlt="macbook pro on black wooden table"
+          githubUrl="https://github.com/martendebruijn/dotfiles"
         />
         <SectionItem
-          title="React"
-          description="A JavaScript library for building user interfaces."
+          title="delete-merged"
+          description="Delete merged branches in a GitHub repository locally"
           image="https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?q=80&w=3615&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           imageAlt="macbook pro on black wooden table"
+          githubUrl="https://github.com/martendebruijn/delete-merged"
         />
       </Section>
       <Footer />

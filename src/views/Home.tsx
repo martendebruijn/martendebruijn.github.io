4f2a7128efc1bdd4b73ef0aef19499448315545f
Marten de Bruijn <martendebruijn@outlook.com>
2025-01-04 05:52:38 +0100

    add readable name

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -23,7 +23,7 @@ export function Home() {
           githubUrl="https://github.com/martendebruijn/dotfiles"
         />
         <SectionItem
-          title="delete-merged"
+          title="Delete merged"
           description="Delete merged branches in a GitHub repository locally"
           image="https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?q=80&w=3615&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           imageAlt="macbook pro on black wooden table"
